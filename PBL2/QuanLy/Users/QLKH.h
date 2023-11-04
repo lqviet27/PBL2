b0660da99eda0975821da8ab6c85fe5573bb8c21
duckkoder <trungduc02042004@gmail.com>
2023-11-04 23:24:23 +0700

    Usermanagement update

--- PBL2/QuanLy/Users/QLKH.h
+++ PBL2/QuanLy/Users/QLKH.h
@@ -1,1 +1,20 @@
-f
+
+#include"../../Object/Users/User.cpp"
+#include"../../Data_structer/Link_list.cpp"
+#include"../../Data_structer/Node.cpp"
+using namespace std;
+class QLKH
+{
+private:
+    Link_list<User> parUser;
+public:
+    QLKH();
+    QLKH(Link_list<User>);
+    QLKH(const QLKH&);
+    ~QLKH();
+    void addUser(const User&);
+    void showUser();
+    User SearchUser(const string&);
+    void ImportFromFile();
+};
+
---
+++ PBL2/QuanLy/Users/test.cpp
@@ -0,0 +1,37 @@
+#include <windows.h>
+#include"QLKH.cpp"
+#include <fstream>
+#include <conio.h>
+#include<iostream>
+using namespace std;
+
+void TextColor(int x)
+{
+     HANDLE color = GetStdHandle(STD_OUTPUT_HANDLE);
+     SetConsoleTextAttribute(color, x);
+}
+
+
+int main(){
+    // ifstream FileIn("../../Database/Bank/Bank.txt", ios_base::in);
+    // if(FileIn.fail()){
+    //     cout<<"Khong tim thay file Bank.txt! Import khong thanh cong"<<endl;
+    // }
+    // else{
+    //     int count=0;
+    //     while(!FileIn.eof()){
+    //         string id, name;
+    //         char separator;
+    //         getline(FileIn, id, '|');
+    //         getline(FileIn, name, '|');
+    //         cout<<id<<" "<<name;
+    //     }
+    //     FileIn.close();
+    // }
+    TextColor(14);
+    QLKH a;
+    // a.showBank();
+    cout<<endl;
+    a.showUser();
+    User b=a.SearchUser("1");
+}
