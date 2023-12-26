d1cf3da6ce0dab1ce8b54a959273d89305e484db
duckkoder <trungduc02042004@gmail.com>
2023-12-26 19:34:41 +0700

    's'

--- PBL2/Admin/Admin.h
+++ PBL2/Admin/Admin.h
@@ -1,5 +1,5 @@
-#include <filesystem>
-namespace fs = std::filesystem;
+// #include <filesystem>
+// namespace fs = std::filesystem;
 #include"../QuanLy/Account/QLTK.cpp"
 #include"../QuanLy/Users/QLKH.cpp"
 #include"../QuanLy/Bank/QLNH.cpp"
@@ -28,8 +28,8 @@ class AdminQLNH{
         static void TraCuuDanhSachTaiKhoan(QLNH&);
         static void TraCuuDanhSachKhachHang(QLNH&);
         static void TongDoanhThu(QLNH&);
-        static void ThongKeSoLuongNapTien(QLNH&);
-        static void ThongKeSoLuongRutTien(QLNH&);
-        static void ThongKeSoLuongChuyenTien(QLNH&);
+        // static void ThongKeSoLuongNapTien(QLNH&);
+        // static void ThongKeSoLuongRutTien(QLNH&);
+        // static void ThongKeSoLuongChuyenTien(QLNH&);
 };
 
